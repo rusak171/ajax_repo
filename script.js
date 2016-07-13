f7ecd2d20ca34e016d86c8d90b3b843700839131
roman <steppehare@gmail.com>
2016-07-13 08:39:22 +0300

    Modify for old browsers HAR-request
    Check for HAR-object (for current browsers) and use ActiveXObject (for old browsers)

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'data.txt', false); //false => sync, true(default) => async
+var request;
+//check for exist HAR-object
+if(window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+} else {
+    request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+request.open('GET', 'data.txt'); //false => sync, true(default) => async
 request.onreadystatechange = function () {
     if (request.readyState === 4 && request.status === 200) {
         console.log(request);
