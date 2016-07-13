91ffcfb11d528d734305151949cec71c3ea27d86
roman <steppehare@gmail.com>
2016-07-13 08:18:10 +0300

    Modify HAR-request from sync to async
    Modify XMLHttpRequest from synchronous to asyncronous and
    check readyState therefore.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-for (var i = 0; i < 1000; ++i) {
-    var request = new XMLHttpRequest();
-    request.open('GET', 'data.txt', false);
-    request.send();
-    if (request.status === 200) {
+var request = new XMLHttpRequest();
+request.open('GET', 'data.txt', false); //false => sync, true(default) => async
+request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
         console.log(request);
         document.writeln(request.responseText);
     }
 }
+request.send();
+
