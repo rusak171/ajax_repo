18d24a447d55c4c41f7bef8584f1d33c7642f646
roman <steppehare@gmail.com>
2016-07-13 09:03:06 +0300

    Use getElementById for modify html-code

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ if(window.XMLHttpRequest) {
 request.open('GET', 'data.txt'); //false => sync, true(default) => async
 request.onreadystatechange = function () {
     if (request.readyState === 4 && request.status === 200) {
-        console.log(request);
-        document.writeln(request.responseText);
+        var modify = document.getElementById("update");
+        modify.innerHTML = request.responseText;        
     }
 }
 request.send();
