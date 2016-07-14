28f98c8c2ee24f9e30f2c816ff624ad3a23c2e63
roman <steppehare@gmail.com>
2016-07-14 14:24:46 +0300

    Read XML-data
    Use responseXML to get data from xml-file

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ if(window.XMLHttpRequest) {
     request = new ActiveXObject("Microsoft.XMLHTTP");
 }
 
-request.open('GET', 'data.txt'); //false => sync, true(default) => async
+request.open('GET', 'data.xml'); //false => sync, true(default) => async
 request.onreadystatechange = function () {
     if (request.readyState === 4 && request.status === 200) {
-        var modify = document.getElementsByTagName('ul')[1]
-                .getElementsByTagName('li');
-        for(var i = 0; i < modify.length; ++i) {
-            modify[i].innerHTML = request.responseText;
+        var modify = document.getElementById('update');
+        var nodeArr = request.responseXML.getElementsByTagName('TITLE');
+        var outputHTML = "<ul>";
+        for(var i = 0; i < nodeArr.length; ++i) {
+            outputHTML += "<li>" + nodeArr[i].firstChild.nodeValue + "</li>";
         }
+        outputHTML += "</ul";
+        modify.innerHTML = outputHTML;
     }
 }
 request.send();
