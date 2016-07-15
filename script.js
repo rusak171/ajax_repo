a0a08c0131ebf039e3344164c5b7e80c2b63f273
roman <steppehare@gmail.com>
2016-07-15 17:23:54 +0300

    Read JSON-data by jQuery

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-$('.update:even').load('data.txt');
+$.getJSON("data.json", function(data) {
+    var output = "<ul>";
+    $.each(data, function (key, val) {
+        output += "<li>" + val.firstName + ' ' + val.lastName + "</li>";
+    });
+    output += "</ul>";
+    //$('#update').html(output); //replase code
+    //$('#update').append(output); //add code to the end
+    $('#update').prepend(output); //add code to the begin
+});
