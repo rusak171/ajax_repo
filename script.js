db89203f4d3d25ab002cc330a1bfdcc0ce84a3f8
roman <steppehare@gmail.com>
2016-07-18 18:54:41 +0300

    Create search system
    Create a simple research system across html-page with css

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-$.getJSON("data.json", function(data) {
-    var output = "<ul>";
-    $.each(data, function (key, val) {
-        output += "<li>" + val.firstName + ' ' + val.lastName + "</li>";
+$("#search").keyup(function () {
+    var searchValue = $("#search").val();
+    var myRegExp = new RegExp(searchValue, "i");
+    //console.log(searchValue);
+    $.getJSON("data.json", function (data) {
+        var output = '<ul class="searchresults">';
+        $.each(data, function (key, val) {
+            if (val.firstName.search(myRegExp) != -1
+                    || val.lastName.search(myRegExp) != -1) {
+                output += "<li>";
+                output += "<h2>" + val.firstName + " " + val.lastName + "</h2>";
+                output += '<img src="images/' + val.image + '" alt="' + val.lastName + '"/>';
+                output += "<h4>" + val.instrument + "</h4>";
+                output += "<p>" + val.text + "</p>";
+                output += "</li>";
+            }
+        });
+        output += "</ul>";
+        $("#update").html(output);
     });
-    output += "</ul>";
-    //$('#update').html(output); //replase code
-    //$('#update').append(output); //add code to the end
-    $('#update').prepend(output); //add code to the begin
 });
+
